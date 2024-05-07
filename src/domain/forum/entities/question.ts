f723293463f0592ce96e8e761c2268b44cf580f7
julio <julio.antonini@gylabs.com.br>
2024-05-06 22:30:08 -0300

    refactor: fix imports

---
+++ src/domain/forum/entities/question.ts
@@ -0,0 +1,86 @@
+import { BaseEntity } from '@/core/entities/base.entity';
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
+import dayjs from 'dayjs';
+
+import { Slug } from '../enterprise/entities/value-objects/slug';
+
+interface IQuestionProps {
+  authorId: UniqueEntityId;
+  title: string;
+  content: string;
+  slug: Slug;
+  bestAnswerId?: UniqueEntityId;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class Question extends BaseEntity<IQuestionProps> {
+  get authorId(): UniqueEntityId {
+    return this.props.authorId;
+  }
+
+  get title(): string {
+    return this.props.title;
+  }
+
+  get content(): string {
+    return this.props.content;
+  }
+
+  get slug(): Slug {
+    return this.props.slug;
+  }
+
+  get bestAnswerId(): UniqueEntityId | undefined {
+    return this.props.bestAnswerId;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3;
+  }
+
+  get excerpt(): string {
+    return this.content.substring(0, 120).trim().concat('...');
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  private touch(): void {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(props: Optional<IQuestionProps, 'createdAt' | 'slug'>, id: UniqueEntityId): Question {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id,
+    );
+    return question;
+  }
+}
