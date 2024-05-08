adaa76423b8580200438adc4002e3c1ede0f1dfe
julio <julio.antonini@gylabs.com.br>
2024-05-08 15:47:07 -0300

    feat: add faker in question mock factory

--- src/test/factories/make-question-mock.factory.ts
+++ src/test/factories/make-question-mock.factory.ts
@@ -1,14 +1,18 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { IQuestionProps, Question } from '@/domain/forum/entities/question';
+import { faker } from '@faker-js/faker';
 
 export class makeQuestionMockFactory {
-  public static create(override: Partial<IQuestionProps> = {}): Question {
-    const question = Question.create({
-      authorId: new UniqueEntityId(),
-      title: 'Test question',
-      content: 'Test question content',
-      ...override,
-    });
+  public static create(override: Partial<IQuestionProps> = {}, id?: UniqueEntityId): Question {
+    const question = Question.create(
+      {
+        authorId: new UniqueEntityId(),
+        title: faker.lorem.sentence(),
+        content: faker.lorem.text(),
+        ...override,
+      },
+      id,
+    );
     return question;
   }
 }
